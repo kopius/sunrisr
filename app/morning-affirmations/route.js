be7b61688b426c463770408f1a112471dd59faa3
kopiusmaximus <alexander.kopynec@gmail.com>
2016-11-06 13:47:39 -0500

    "Start Affirming" button displays first quiz
    - add "Start Affirming" button to top-level template on the
    morning-affirmations route
    - clicking the button fires an action that finds the first morning
    affirmation with { completed: false }, and gives it the new attribute
    of { isActive: true }
    - morning-affirmations template listens for isActive on morning
    affirmations and renders a sunrisr-quiz component if true

--- app/morning-affirmations/route.js
+++ app/morning-affirmations/route.js
@@ -6,5 +6,18 @@ export default Ember.Route.extend({
     return this.get('store').findAll('morningAffirmation');
     // console.log('model is:', model);
     // return model;
+  },
+
+  actions: {
+    // find the first incomplete morningAffirmation and set it to active
+    startAffirming(model) {
+      console.log('in startAffirming on the morning-affirmations route');
+      let nextAffirmation = model.find((morningAffirmation) => {
+        return morningAffirmation.get('completed') === false;
+      });
+      nextAffirmation.set('isActive', true);
+      console.log('nextAffirmation.isActive is', nextAffirmation.get('isActive'));
+    },
   }
+
 });
