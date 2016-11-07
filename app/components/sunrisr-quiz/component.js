463e4e73a5094560d1f3dafd3323b89c39a603c7
kopiusmaximus <alexander.kopynec@gmail.com>
2016-11-06 21:21:59 -0500

    Correct response advances to next affirmation
    When a correct response is submitted:
    - the active morningAffirmation is marked as complete
    - the next incomplete morningAffirmation is marked as active
    - the input form is cleared
    - if there are no incomplete morningAffirmations left, the current
    morning is marked as complete
    
    TODO:
    - render all morningAffirmations as separate components
    - add success & failure messages to the UI
    - keep a running list of completed prompt-response pairs below
    the active morningAffirmation
    - call .save() on completed MA's and mornings to persist changes to API

--- app/components/sunrisr-quiz/component.js
+++ app/components/sunrisr-quiz/component.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  form: {
+    response: null
+  },
+
+  actions: {
+    checkMatch(morningAffirmation) {
+      console.log('in checkMatch on sunrisr-quiz component');
+      let form = this.get('form');
+      let response = form.response;
+      console.log("response is", response);
+      console.log('morningAffirmation is', morningAffirmation);
+
+      this.sendAction('checkMatch', response, morningAffirmation);
+      this.set('form.response', null);
+    }
+  }
 });
--- app/morning-affirmations/route.js
+++ app/morning-affirmations/route.js
@@ -8,16 +8,108 @@ export default Ember.Route.extend({
     // return model;
   },
 
+  // this method does not currently work
+  getCurrentMorning() {
+    console.log('in getCurrentMorning. this is:', this);
+    // get all mornings
+    let mornings = this.get('store').findAll('morning');
+
+    // get parameters for comparison
+    let currentDate = new Date().getDate();
+    let currentMonth = new Date().getMonth();
+    let currentYear = new Date().getYear();
+
+    // check mornings for a matching date
+    mornings.forEach((morning) => {
+      // get parameters for comparison
+      let date = morning.createdAt.getDate();
+      let month = morning.createdAt.getMonth();
+      let year = morning.createdAt.getYear();
+
+      if (date === currentDate &&
+          month === currentMonth &&
+          year === currentYear) {
+        console.log('this morning is a match! returning:', morning);
+        return morning;
+      }
+    });
+  },
+
+  activateNextMorningAffirmation() {
+    console.log('in activateNextMorningAffirmation');
+    let model = this.controller.get('model');
+    // TODO: this code is repeated in a couple of places - factor it out at some point
+    let nextMA = model.find((morningAffirmation) => {
+      return morningAffirmation.get('completed') === false;
+    });
+    if (nextMA) {
+      nextMA.set('isActive', true);
+      console.log('nextMA is', nextMA);
+      return true;
+    } else {
+      console.log('there is no nextMA');
+      return false;
+    }
+  },
+
   actions: {
     // find the first incomplete morningAffirmation and set it to active
     startAffirming(model) {
       console.log('in startAffirming on the morning-affirmations route');
-      let nextAffirmation = model.find((morningAffirmation) => {
+      let nextMA = model.find((morningAffirmation) => {
         return morningAffirmation.get('completed') === false;
       });
-      nextAffirmation.set('isActive', true);
-      console.log('nextAffirmation.isActive is', nextAffirmation.get('isActive'));
+      nextMA.set('isActive', true);
+
+      // tickle the current morning
+      let currentMorning = nextMA.get('morning');
+      console.log('ticking the current morning:', currentMorning);
+
+      console.log('nextMA.isActive is', nextMA.get('isActive'));
     },
+
+    checkMatch(response, morningAffirmation) {
+      console.log('in checkMatch on the morning-affirmations route');
+      console.log('response is', response);
+      let affirmation = morningAffirmation.get('affirmation');
+      console.log('affirmation is', affirmation);
+
+      // Shouldn't need this code anymore - now passing the whole MA in:
+      // get the active MA
+      // let activeMA = this.get('store').query('morningAffirmation', { isActive: true });
+      // let activeMA = this.get('store').query('morningAffirmation', 1);
+      // console.log('activeMA is', activeMA);
+
+      // check to see if response matches
+      if (affirmation.get('response') === response) {
+        // if so:
+        console.log('response is a match! yaaaaaay!');
+        // find the active MA and set its `completed` to true and `isActive` to false
+        morningAffirmation.set('completed', true);
+        morningAffirmation.set('isActive', false);
+
+
+        // check to see if all MA's are completed
+        let currentMorning = morningAffirmation.get('morning'); /*FIXME*/
+        console.log('back in checkMatch, currentMorning is:', currentMorning);
+
+        if (!this.activateNextMorningAffirmation()) {
+          currentMorning.set('completedAll', true);
+          console.log('back in checkMatch, all morning affirmations complete');
+        }
+        return true;
+      } else {
+        console.log('NO MATCHY');
+        return false;
+      }
+
+
+
+
+      //        - if yes, get the current Morning and set its `completed_all` = true
+      //        - if no, get the next MA and set its `isActive` to true
+
+    }
   }
 
 });
