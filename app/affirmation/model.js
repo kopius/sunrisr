116d48d4f68dbf509a968d14ad29aa0e457dcd66
kopiusmaximus <alexander.kopynec@gmail.com>
2016-11-08 16:01:08 -0500

    Fix has-many relationship bug
    - the has-many relationship with morning-affirmations was not correctly
    referenced in the model for either owning entity (affirmation or
    morning). This commit fixes this.

--- app/affirmation/model.js
+++ app/affirmation/model.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   prompt: DS.attr('string'),
-  response: DS.attr('string')
+  response: DS.attr('string'),
+  morningAffirmations: DS.hasMany('morning-affirmation')
 });
--- app/morning/model.js
+++ app/morning/model.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   completedAll: DS.attr('boolean'),
   createdAt: DS.attr('date'),
-  hasMany: DS.attr('morning-affirmation')
+  morningAffirmations: DS.hasMany('morning-affirmation')
 });
