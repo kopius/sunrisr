0165c19d90849a8efce87ce3b556a177c23cea75
kopiusmaximus <alexander.kopynec@gmail.com>
2016-11-08 12:08:49 -0500

    If not authenticated '/' redirects to sign-in
    - an un-authenticated user attempting to access any route beginning with
    '/' (which should be all routes) will be redirected to '/sign-in'

--- app/index/route.js
+++ app/index/route.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
+
+  beforeModel() {
+    if (!this.get('isAuthenticated')) {
+      this.transitionTo('sign-in');
+    }
+  }
 });
