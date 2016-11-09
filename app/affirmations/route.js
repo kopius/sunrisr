cca4b7ec121e823971a20359718a0f8c049a45a3
kopiusmaximus <alexander.kopynec@gmail.com>
2016-11-08 23:16:43 -0500

    Add form component for editing affirmation
    - Create a component for editing an affirmation
    - Clicking 'edit' in a sunrisr-affirmation now re-renders the same
    affirmation inside a sunrisr-affirmation-form component
    - outline the actions that need to be defined on each component and
    on the parent route that will primarily be using the components

--- app/affirmations/route.js
+++ app/affirmations/route.js
@@ -3,5 +3,37 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model () {
     return this.get('store').findAll('affirmation');
+  },
+
+  form: {
+    prompt: null,
+    response: null,
+  },
+
+  actions: {
+    /* TODO: Finish and test this action. Ensure the new record persists. */
+    createAffirmation() {
+      let newAffirmation = this.get('form');
+      let affirmation = this.get('store').createRecord('affirmation',
+                                                       newAffirmation);
+      affirmation.save();
+
+      this.set('form.prompt', null);
+      this.set('form.response', null);
+    },
+
+    editAffirmation(affirmation) {
+      /* TODO */
+      console.log('in editAffirmation on affirmations route, affirmation is:', affirmation);
+      affirmation.set('isEditing', true);
+    },
+
+    deleteAffirmation(affirmation) {
+      /* TODO */
+      console.log('in deleteAffirmation on affirmations route, affirmation is:', affirmation);
+      affirmation.deleteRecord();
+      console.log('affirmation was deleted?', affirmation.get('isDeleted')); // => true
+      affirmation.save(); // => DELETE to /affirmations/1
+    }
   }
 });
